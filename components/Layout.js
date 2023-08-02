5ebeb55983aded2a5090e2ab5e2092ea879d2824
atsumu22 <w.atsumu27@gmail.com>
2023-08-02 16:29:53 +0900

    set data fetching and prerendering

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,20 @@
 import Head from "next/head";
 
+import styles from "./Layout.module.css"
+import utilStyles from "../styles/utils.module.css"
+
 const name = "atsumu code"
 export const siteTitle = "Next.js blog";
 
 function Layout({children}) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/images/profile.png" />
       </Head>
-      <header>
-        <img src="/images/profile.png" />
-        <h1>{name}</h1>
+      <header className={styles.header}>
+        <img src="/images/profile.png" className={utilStyles.borderCircle} />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,51 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
 import Link from "next/link";
-import Layout from '@/components/Layout';
 
-export default function Home() {
+import styles from '@/styles/Home.module.css'
+import Layout from '@/components/Layout';
+import utilStyles from "../styles/utils.module.css";
+import { getPostsData } from '@/lib/post';
+
+// SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+  console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
-   <Layout>ここに動的なコンテンツがきます</Layout>
+   <Layout>
+    <section className={utilStyles.headingMd}>
+      <p>
+        私はジュニアフルスタックエンジニアです。Ruby on Railsを用いたアプリケーション開発。Reactを用いたフロントエンド開発をメインとしています。現在Next.jsを学んでいます。AWSやDockerなどインフラ側を苦手としています。
+      </p>
+    </section>
+
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2>📝エンジニアのブログ</h2>
+      <div className={styles.grid}>
+        {allPostsData.map(({ id, title, date, thumbnail }) => (
+          <article key={id}>
+            <Link href={`/posts/${id}`}>
+              <img src={thumbnail} className={styles.thumbnailImage}/>
+            </Link>
+            <Link legacyBehavior href={`/posts/${id}`}>
+              <a className={utilStyles.boldText}>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>{date}</small>
+          </article>
+        ))}
+      </div>
+    </section>
+   </Layout>
   )
 }
